b613841c1cb36215018d2da107f7cebf836e8af1
YuYin <yguan5@ucsc.edu>
2014-04-03 18:31:59 -0700

    UART Code/ Arduino side
    This is a example and test on how the UART will work, tested on UNO.

---
+++ UART code/ArduinoSide/ArduinoSide.ino
@@ -0,0 +1,17 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(7, 6); // RX, TX
+
+void setup()  
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+}
+
+void loop() // run over and over
+{
+  if (mySerial.available()) Serial.write(mySerial.read());
+  if (Serial.available()) mySerial.write(Serial.read());
+}
